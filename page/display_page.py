1d2cb358590e4bcc18e7ae142f81beacd0182957
hitheima <hitheima@163.com>
2018-06-25 12:02:10 +0800

    分离测试脚本和页面 - display部分，新建了page模块。page用来提供动作，script用来调用动作，page注重做什么事情，script注重按什么顺序去做事情。

---
+++ page/display_page.py
@@ -0,0 +1,16 @@
+class DisplayPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click_display(self):
+        self.driver.find_element_by_xpath("//*[contains(@text,'显示')]").click()
+
+    def click_search(self):
+        self.driver.find_element_by_id("com.android.settings:id/search").click()
+
+    def input_keyword(self):
+        self.driver.find_element_by_id("android:id/search_src_text").send_keys("hello")
+
+    def click_back(self):
+        self.driver.find_element_by_class_name("android.widget.ImageButton").click()
--- scripts/test_display.py
+++ scripts/test_display.py
@@ -1,12 +1,19 @@
 from base import init_driver
+from page.display_page import DisplayPage
 
 class TestDisplay:
 
     def setup(self):
         self.driver = init_driver()
+        self.display_page = DisplayPage(self.driver)
 
     def test_display_search(self):
-        self.driver.find_element_by_xpath("//*[contains(@text,'显示')]").click()
-        self.driver.find_element_by_id("com.android.settings:id/search").click()
-        self.driver.find_element_by_id("android:id/search_src_text").send_keys("hello")
-        self.driver.find_element_by_class_name("android.widget.ImageButton").click()
+        self.display_page.click_display()
+        self.display_page.click_search()
+        self.display_page.input_keyword()
+        self.display_page.click_back()
+
+    # def test_display_search1(self):
+    #     self.display_page.click_search()
+    #     self.display_page.input_keyword()
+    #     self.display_page.click_back()
