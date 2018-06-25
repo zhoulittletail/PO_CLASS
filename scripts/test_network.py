26fed15114662dfe861359ebe198ab75a786ce16
hitheima <hitheima@163.com>
2018-06-25 10:51:14 +0800

    封装前置代码，把前置代码放在了base/base_driver中。

--- scripts/test_network.py
+++ scripts/test_network.py
@@ -1,24 +1,10 @@
-from appium import webdriver
+from base.base_driver import init_driver
 
 
 class TestNetwrok:
 
     def setup(self):
-        # server 启动参数
-        desired_caps = dict()
-        # 设备信息
-        desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '5.1'
-        desired_caps['deviceName'] = '192.168.56.101:5555'
-        # app的信息
-        desired_caps['appPackage'] = 'com.android.settings'
-        desired_caps['appActivity'] = '.Settings'
-        # 解决输入中文
-        desired_caps['unicodeKeyboard'] = True
-        desired_caps['resetKeyboard'] = True
-
-        # 声明我们的driver对象
-        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+        self.driver = init_driver()
 
     def test_network_2g(self):
         self.driver.find_element_by_xpath("//*[contains(@text,'更多')]").click()
