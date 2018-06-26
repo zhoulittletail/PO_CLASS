a68329cd7c7416790a54cb56d6b77f94d1d9cde0
hitheima <hitheima@163.com>
2018-06-26 09:36:44 +0800

    抽取点击和输入到page。

--- page/display_page.py
+++ page/display_page.py
@@ -12,16 +12,38 @@ class DisplayPage:
         self.driver = driver
 
     def click_display(self):
-        self.find_element(self.display_button).click()
+        self.click(self.display_button)
 
     def click_search(self):
-        self.find_element(self.search_button).click()
+        self.click(self.search_button)
 
     def input_keyword(self, content):
-        self.find_element(self.search_edit_text).send_keys(content)
+        self.input(self.search_edit_text, content)
 
     def click_back(self):
-        self.find_element(self.back_button).click()
+        self.click(self.back_button)
+
+    def click(self, feature):
+        """
+        根据某个特征，进行查找并且点击
+        :param feature: 特征
+        :return:
+        """
+        self.find_element(feature).click()
+
+    def input(self, feature, text):
+        """
+        根据某个特征，进行查找并且输入对应的文字
+        :param feature: 特征
+        :param text: 文字
+        :return:
+        """
+        self.find_element(feature).send_keys(text)
 
     def find_element(self, feature):
+        """
+        根据特征，找元素
+        :param feature: 特征
+        :return: 元素
+        """
         return self.driver.find_element(feature[0], feature[1])
--- page/network_page.py
+++ page/network_page.py
@@ -13,19 +13,41 @@ class NetwrokPage:
         self.driver = driver
 
     def click_more(self):
-        self.find_element(self.more_button).click()
+        self.click(self.more_button)
 
     def click_mobile_network(self):
-        self.find_element(self.mobile_network_button).click()
+        self.click(self.mobile_network_button)
 
     def click_first_network(self):
-        self.find_element(self.first_network_button).click()
+        self.click(self.first_network_button)
 
     def click_2g_network(self):
-        self.find_element(self.network_2g_button).click()
+        self.click(self.network_2g_button)
 
     def click_3g_network(self):
-        self.find_element(self.network_3g_button).click()
+        self.click(self.network_3g_button)
+
+    def click(self, feature):
+        """
+        根据某个特征，进行查找并且点击
+        :param feature: 特征
+        :return:
+        """
+        self.find_element(feature).click()
+
+    def input(self, feature, text):
+        """
+        根据某个特征，进行查找并且输入对应的文字
+        :param feature: 特征
+        :param text: 文字
+        :return:
+        """
+        self.find_element(feature).send_keys(text)
 
     def find_element(self, feature):
+        """
+        根据特征，找元素
+        :param feature: 特征
+        :return: 元素
+        """
         return self.driver.find_element(feature[0], feature[1])
